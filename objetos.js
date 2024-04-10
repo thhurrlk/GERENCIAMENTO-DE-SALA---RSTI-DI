49e318a3b5851fc406375f3538bb2dfccd112d31
thhurrlk <robot24master@gmail.com>
2024-04-10 16:45:39 -0300

    codigo 90% completo

---
+++ objetos.js
@@ -0,0 +1,6 @@
+var database = {
+    quartosdb: [{ id: 1, tipo: 'VIP', descricao: 'Quarto VIP: Espaçoso, confortável e com vista panorâmica.', disponibilidade: 1 }, { id: 2, tipo: 'VIP', descricao: 'Quarto VIP: Espaçoso, confortável e com vista panorâmica.', disponibilidade: 1 }, {id: 3, tipo: 'Comum', descricao: 'Quarto Comum: Simples e confortável.', disponibilidade: 1}],
+    reservasdb: [{}]
+}
+
+export { database };    
